03322afeaba214256247ce6a09253d505bb7f558
lipsasharma <79360743+LIPSASHARMA@users.noreply.github.com>
2022-11-29 22:13:28 -0500

    last7daysearthquake data

--- Earthquakes_past7days/static/js/logicStep1.js
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -1,37 +1,6 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-
-// Create the map object with center at the San Francisco airport.
-//let map = L.map('mapid').setView([37.5, -122.5], 10);
-// Create the map object with center and zoom level.
-//let map = L.map('mapid').setView([30, 30], 2);
-
-
-
-
-
-// Grabbing our GeoJSON data.
-/*
-L.geoJSON(sanFranAirport, {
-  // We turn each feature into a marker on the map.
-  pointToLayer: function(feature, latlng) {
-    console.log(feature);
-    return L.marker(latlng)
-    .bindPopup("<h2>" + feature.properties.city + "</h2>");
-  }
-
-}).addTo(map);
-*/
-/*
-L.geoJSON(sanFranAirport, {
-  // We turn each feature into a marker on the map.
-  onEachFeature: function (feature, layer) {
-    console.log(layer);
-    layer.bindPopup();
-  }
-}).addTo(map);
-*/
 // We create the dark view tile layer that will be an option for our map.
 
 
@@ -54,14 +23,14 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 // Create a base layer that holds both maps.
 let baseMaps = {
   "Streets": streets,
-  "Satellite Streets": satelliteStreets
+  "Satellite": satelliteStreets
 };
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-  center: [43.7, -79.3],
-  zoom: 11,
-  layers: [satelliteStreets]
+  center: [39.5, -98.5],
+  zoom: 3,
+  layers: [streets]
 });
 // Then we add our 'graymap' tile layer to the map.
 //streets.addTo(map);
@@ -70,7 +39,7 @@ let map = L.map('mapid', {
 L.control.layers(baseMaps).addTo(map);
 
 // Accessing the Toronto airline routes GeoJSON URL.
-let torontoHoods = "https://raw.githubusercontent.com/LIPSASHARMA/Mapping_Earthquakes/main/torontoNeighborhoods.json";
+//let torontoHoods = "https://raw.githubusercontent.com/LIPSASHARMA/Mapping_Earthquakes/main/torontoNeighborhoods.json";
 
 // Create a style for the lines.
 let myStyle = {
@@ -78,27 +47,10 @@ let myStyle = {
   weight: 2
 }
 
-/*
-// Grabbing our GeoJSON data.
-d3.json(torontoHoods).then(function(data) {
-  console.log(data);
-// Creating a GeoJSON layer with the retrieved data.
-L.geoJSON(data, {
-  style: myStyle,
-  color: "#ffffa1",
-  weight: 2,
-  onEachFeature: function(feature, layer) {
-    layer.bindPopup("<h3>" + "Airline: " + feature.properties.airline +
-    "</h3><hr><h3> Destination: " + feature.properties.dest + "</h3>");
-  }
-}).addTo(map);
-});
-*/
-
-d3.json(torontoHoods).then(function(data) {
-  console.log(data);
-// Creating a GeoJSON layer with the retrieved data.
-L.geoJSON(data).addTo(map);
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data).addTo(map);
 });
 
 /*
