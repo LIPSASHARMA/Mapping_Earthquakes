0f0c3d8a32594918e28bd5b527e9ec94df275aa0
lipsasharma <79360743+LIPSASHARMA@users.noreply.github.com>
2022-11-29 20:16:52 -0500

    mapping multiple points

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -24,7 +24,11 @@ let sanFranAirport =
 // Create the map object with center at the San Francisco airport.
 //let map = L.map('mapid').setView([37.5, -122.5], 10);
 // Create the map object with center and zoom level.
-let map = L.map('mapid').setView([30, 30], 2);
+//let map = L.map('mapid').setView([30, 30], 2);
+
+
+
+
 
 // Grabbing our GeoJSON data.
 /*
@@ -38,7 +42,7 @@ L.geoJSON(sanFranAirport, {
 
 }).addTo(map);
 */
-
+/*
 L.geoJSON(sanFranAirport, {
   // We turn each feature into a marker on the map.
   onEachFeature: function (feature, layer) {
@@ -46,21 +50,46 @@ L.geoJSON(sanFranAirport, {
     layer.bindPopup();
   }
 }).addTo(map);
+*/
+// We create the dark view tile layer that will be an option for our map.
 
 
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/streets-v11',
+    id: 'mapbox/light-v10',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: API_KEY
 });
+
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+  Street: light,
+  Dark: dark
+};
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+  center: [44.0, -80.0],
+  zoom: 2,
+  layers: [light]
+});
 // Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+//streets.addTo(map);
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
 
 // Accessing the airport GeoJSON URL
 let airportData = "https://raw.githubusercontent.com/LIPSASHARMA/Mapping_Earthquakes/main/majorAirports.json";
+
 
 // Grabbing our GeoJSON data.
 d3.json(airportData).then(function(data) {
@@ -74,3 +103,4 @@ L.geoJson(data,{
   }    
 }).addTo(map);
 });
+
